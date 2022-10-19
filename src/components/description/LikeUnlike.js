d204aab539273e6fbdad151ae29c5c2f41519eab
Mosnur Ahmed <mosnurahmed01@gmail.com>
2022-10-20 01:31:46 +0600

    like unlike problem solve

--- src/components/description/LikeUnlike.js
+++ src/components/description/LikeUnlike.js
@@ -1,32 +1,25 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import likeImage from "../../assets/like.svg";
 import unlikeImage from "../../assets/unlike.svg";
 import { countLikeUnlike } from "../../features/likeUnlike/likeUnlikeSlice";
 
 export default function LikeUnlike({ id, likes, unlikes }) {
-  const [like, setLike] = useState(likes);
-  const [unlike, setUnlike] = useState(unlikes);
-
-  
-  const {video} = useSelector((state) => state.likeUnlike);
-  console.log(`videoLIke:${video.likes}`);
   const dispatch = useDispatch();
-  console.log(`"stateLike:${like} "`);
+
+  useEffect(() => {
+    dispatch(countLikeUnlike({ id }));
+  }, [id, dispatch]);
+
+  const { video } = useSelector((state) => state.likeUnlike);
+  console.log(`videoLIke:${video.likes}`);
 
   const likeHandler = (reaction, id) => {
-    dispatch(countLikeUnlike({ reaction, id }));
-    // setLike((prevLike) => {
     
-    //   prevLike = video.video.likes;
-    //   console.log(`previous :${prevLike}`);
-    //   return video.video.likes;
-    // });
-    setLike(video.likes)
+    dispatch(countLikeUnlike({ reaction, id }));
   };
   const unpLikeHandler = (reaction, id) => {
     dispatch(countLikeUnlike({ reaction, id }));
-    setUnlike(video.unlikes)
   };
 
   return (
@@ -39,10 +32,9 @@ export default function LikeUnlike({ id, likes, unlikes }) {
             alt="Like"
             onClick={() => {
               likeHandler("like", id);
-              // setUnlike(video.video.unlikes);
             }}
           />
-          <span>{like} </span>
+          <span>{video.likes} </span>
         </div>
         <div className="text-sm leading-[1.7142857] text-slate-600"></div>
       </div>
@@ -53,12 +45,10 @@ export default function LikeUnlike({ id, likes, unlikes }) {
             src={unlikeImage}
             alt="Unlike"
             onClick={() => {
-              // setReaction("unlikes");
-
               unpLikeHandler("unlike", id);
             }}
           />
-          <span>{unlike}</span>
+          <span>{video.unlikes}</span>
         </div>
         <div className="text-sm leading-[1.7142857] text-slate-600"></div>
       </div>
--- src/features/likeUnlike/likeUnlikeAPI.js
+++ src/features/likeUnlike/likeUnlikeAPI.js
@@ -1,30 +1,25 @@
 import axios from "../../utils/axios";
 
 export const updateLikeUnlike = async (reaction, id) => {
-  console.log(reaction);
-  console.log(id);
-  // const { data } = await axios.get(`/videos/${id}`);
-  const { data } = await axios.get(`/videos/${id}`);
-
-  if (data) {
-    let updateReaction =
-      reaction === "like"
-        ? {
-            likes: data.likes + 1,
-          }
-        : {
-            unlikes: data.unlikes + 1,
-          };
-
-    const response = await axios.patch(`/videos/${id}`, updateReaction);
-    // console.log(response);
+  if (reaction === undefined) {
+    const response = await axios.get(`/videos/${id}`);
     return response.data;
+  } else {
+    const { data } = await axios.get(`/videos/${id}`);
+
+    if (data) {
+      let updateReaction =
+        reaction === "like"
+          ? {
+              likes: data.likes + 1,
+            }
+          : {
+              unlikes: data.unlikes + 1,
+            };
+
+      const response = await axios.patch(`/videos/${id}`, updateReaction);
+
+      return response.data;
+    }
   }
 };
-
-// export const updateLikeUnlike = async (reaction, id) => {
-//   const response = await axios.get(`/videos/${id}`);
-//   console.log(response)
-//   return response.data;
-
-// };
